c7724cde04303f6ebc93436c3b8c9c1a76e4ff86
Luan Felipe <luanfelipebtu@hotmail.com>
2019-02-23 22:31:53 -0300

    Listando e trocando mensagens

--- app/src/main/java/com/example/luan/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/example/luan/whatsapp/activity/ChatActivity.java
@@ -2,9 +2,13 @@ package com.example.luan.whatsapp.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.view.View;
@@ -14,12 +18,19 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.luan.whatsapp.R;
+import com.example.luan.whatsapp.adapter.MensagensAdapter;
 import com.example.luan.whatsapp.config.ConfiguracaoFirebase;
 import com.example.luan.whatsapp.helper.Base64Custom;
 import com.example.luan.whatsapp.helper.UsuarioFirebase;
 import com.example.luan.whatsapp.model.Mensagem;
 import com.example.luan.whatsapp.model.Usuario;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,6 +41,12 @@ public class ChatActivity extends AppCompatActivity {
     private Usuario usuarioDestinatario;
     private EditText editMsg;
     private FloatingActionButton fabEnviarMsg;
+    private RecyclerView recyclerMensagens;
+    private MensagensAdapter mensagensAdapter;
+    private List<Mensagem> mensagens = new ArrayList<>();
+    private DatabaseReference database;
+    private DatabaseReference mensagensRef;
+    private ChildEventListener childEventListenerMensagens;
 
     //identificador usuarios remetente e destinatario
     private String idUsuarioRemetente;
@@ -41,15 +58,19 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        //Configurar Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //Configuracoes Iniciais
         textNomeChat = findViewById(R.id.textNomeChat);
         imageFotoChat = findViewById(R.id.circleImageFotoChat);
         editMsg = findViewById(R.id.editMensagem);
         fabEnviarMsg = findViewById(R.id.fabEnviarMsg);
+        recyclerMensagens = findViewById(R.id.recyclerMensagens);
 
         //recuperar dados do usuario remetente
         idUsuarioRemetente = UsuarioFirebase.getIdUsuario();
@@ -70,18 +91,24 @@ public class ChatActivity extends AppCompatActivity {
             }else {
                 imageFotoChat.setImageResource(R.drawable.padrao);
             }
-
             //Recuperar dados usuario destinatario
             idUsuarioDestinatario = Base64Custom.codificadorBase64(usuarioDestinatario.getEmail());
         }
 
+        //Configurar adapter
+        mensagensAdapter = new MensagensAdapter(mensagens,getApplicationContext());
 
-        fabEnviarMsg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enviarMensagem();
-            }
-        });
+        //Configuração recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMensagens.setLayoutManager(layoutManager);
+        recyclerMensagens.setHasFixedSize(true);
+        recyclerMensagens.setAdapter(mensagensAdapter);
+
+        //Recuperar Mensagens
+        database = ConfiguracaoFirebase.getFirebaseDatabase();
+        mensagensRef = database.child("mensagens")
+                .child(idUsuarioRemetente)
+                .child(idUsuarioDestinatario);
     }
 
     private void enviarMensagem(){
@@ -90,16 +117,19 @@ public class ChatActivity extends AppCompatActivity {
         if(!textoMensagem.isEmpty()){
 
             Mensagem mensagem = new Mensagem();
-
             mensagem.setIdUsuario( idUsuarioRemetente );
             mensagem.setMensagem( textoMensagem );
 
             //Salvar mensagem para o remetente
             salvarMensagem(idUsuarioRemetente,idUsuarioDestinatario,mensagem);
 
+            //Salvar mensagem para o destinario
+            salvarMensagem(idUsuarioDestinatario,idUsuarioRemetente,mensagem);
+
         }else{
-            Toast.makeText(ChatActivity.this,"Digite uma" +
-                    "mensagem para enviar!",Toast.LENGTH_LONG).show();
+            Toast.makeText(ChatActivity.this,
+                    "Digite uma mensagem para enviar!",
+                    Toast.LENGTH_LONG).show();
         }
 
     }
@@ -116,6 +146,60 @@ public class ChatActivity extends AppCompatActivity {
 
         //Limpar texto
         editMsg.setText("");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        recuperarMensagens();
+
+        fabEnviarMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarMensagem();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mensagensRef.removeEventListener(childEventListenerMensagens);
+    }
+
+    private void recuperarMensagens(){
+
+        childEventListenerMensagens = mensagensRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Mensagem mensagem = dataSnapshot.getValue(Mensagem.class);
+                mensagens.add(mensagem);
+                mensagensAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
 }
